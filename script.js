8a516eb3797f47b6a2231feb04b2c1cec3565453
Daniel-Derhami <daniel.derhami@gmail.com>
2023-03-10 16:54:09 +0100

    JS

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
-const fromText = document.querySelector('.from-text'),
+const fromText = document.querySelector(".from-text"),
 toText = document.querySelector('.to-text'),
-exchangeIcon = document.querySelector('.exchagne'),
-selectTag = document.querySelector('select'),
-icons = document.querySelector('.row i'),
-translateBtn = document.querySelector('button'),
+exchangeIcon = document.querySelector(".exchagne"),
+selectTag = document.querySelectorAll("select"),
+icons = document.querySelectorAll(".row i"),
+translateBtn = document.querySelector("button"),
+
+
+selectTag.forEach((tag,id)=>{
+    for (let country_code in countries){
+        let selected;
+        if (id==0 && country_code=='en-GB')
+            selected="selected";
+    }
+    else if(id==1 && country_code=='fa-IR'){
+        selected="selected";
+        }
+    let option = <option ${selected} value="${country_code}">${countries{country_code}}</option>
+})
